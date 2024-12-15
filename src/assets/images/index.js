e87b12cfa1167da51ef38746b9e134e174e6cac7
Austen Anop <austen626@outlook.com>
2024-12-14 19:16:21 -0500

    Added all projects and companies

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,8 +2,14 @@ import hero from './hero.jpg'
 import meta from './meta.png'
 import kartonmedya from './kartonmedya.png'
 import verisoft from './verisoft.png'
+import artburst from './artburst.jpeg'
+import dotlaunch from './dotlaunch.jpeg'
+import levata from './levata.jpeg'
 import okttrailer from './okttrailer.png'
 import logo from './logo.svg'
+import cloudbadging from './cloudbadging.ico'
+import barcodes from './barcodes.jpg'
+import pactum from './pactum.png'
 
 export {
     hero,
@@ -11,5 +17,11 @@ export {
     kartonmedya,
     verisoft,
     okttrailer,
-    logo
+    logo,
+    artburst,
+    dotlaunch,
+    levata,
+    cloudbadging,
+    barcodes,
+    pactum
 }
